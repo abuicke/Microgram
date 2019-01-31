e009bfe537670a46ee8969a1fee7101528bf9c93
abuicke <mosesjayjayjay@gmail.com>
2019-01-31 15:59:59 +0000

    storing access token locally

--- app/src/main/java/ie/gravitycode/microgram/MainActivity.kt
+++ app/src/main/java/ie/gravitycode/microgram/MainActivity.kt
@@ -1,13 +1,16 @@
 package ie.gravitycode.microgram
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import ie.gravitycode.instagram.api.InstagramApi
 import ie.gravitycode.instagram.reponse.userinfo.UserInfo
+import ie.gravitycode.login.util.Authentication
 import ie.gravitycode.microgram.dependencyinjection.DaggerActivityComponent
 import ie.gravitycode.microgram.dependencyinjection.MvcViewModule
+import io.reactivex.Maybe
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
@@ -31,18 +34,29 @@ class MainActivity : AppCompatActivity() {
     private val instagramOAuthMvcView by lazy { mvcViewFactory.getInstagramOAuthMvcView(null) }
     private val userProfileMvcView by lazy { mvcViewFactory.getUserProfileMvcView(null) }
 
-    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityComponent.inject(this)
 
-        loginMvcView.subscribeLoginClicked()
-            .switchMap {
-                setContentView(instagramOAuthMvcView.getRootView())
-                instagramOAuthMvcView.startSignIn()
-            }
+        val storedAuthSource = getStoredAccessToken()
+            .toObservable()
+
+        val userLoginAuthSource =
+            loginMvcView.subscribeLoginClicked()
+                .observeOn(AndroidSchedulers.mainThread())
+                .switchMap {
+                    setContentView(instagramOAuthMvcView.getRootView())
+                    instagramOAuthMvcView.startSignIn()
+                }
+
+        Observable.concat(storedAuthSource, userLoginAuthSource)
             .observeOn(Schedulers.io())
-            .switchMap { auth -> instagramApi.getUserInfo(auth.getAccessToken()) }
+            .firstElement()
+            .flatMapObservable { auth ->
+                val accessToken = auth.getAccessToken()
+                storeAccessToken(accessToken)
+                instagramApi.getUserInfo(accessToken)
+            }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(object : DisposableObserver<UserInfo>() {
                 override fun onComplete() {
@@ -63,6 +77,25 @@ class MainActivity : AppCompatActivity() {
             })
 
         setContentView(loginMvcView.getRootView())
+    }
+
+    private fun storeAccessToken(accessToken: String) {
+        getPreferences(MODE_PRIVATE).edit {
+            putString("access_token", accessToken)
+            apply()
+        }
+    }
+
+    private fun getStoredAccessToken(): Maybe<Authentication> {
+        val accessToken = getPreferences(MODE_PRIVATE)
+            .getString("access_token", null)
+        return if (accessToken == null) {
+            Maybe.empty<Authentication>()
+        } else {
+            Maybe.just(object : Authentication {
+                override fun getAccessToken() = accessToken
+            })
+        }
     }
 
 }
